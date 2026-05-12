e957d706fe853fb43a499dd123355347f8e8b0c4
Pritam <itspratap255@gmail.com>
2026-05-12 08:48:48 +0530

    complete all pages (Terms,About,Privacy-Policy,Contact) with active url routing on home page

--- home/views.py
+++ home/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/home.html')
+
+def about(request):
+    return render(request, 'home/about_us.html')
+
+def contact_us(request):
+    return render(request, 'home/get_support.html')
+
+def terms(request):
+    return render(request, 'home/terms.html')
+
+def privacy_policy(request):
+    return render(request, 'home/privacy_policy.html')
